817e0cde91750b4d979a0650970145971f046fcc
Emanuele Canessa <ema0185@gmail.com>
2015-08-26 01:41:41 +0200

    xGetSAD16: unroll prologue

--- source/Lib/TLibCommon/TComRdCost.cpp
+++ source/Lib/TLibCommon/TComRdCost.cpp
@@ -629,13 +629,15 @@ Distortion TComRdCost::xGetSAD16( DistParam* pcDtParam )
     piCur += iStrideCur;
   }
 
-  Distortion uiSum = 0;
-  for (int i = 0; i < 4; i++)
-  {
-    uiSum +=
-      vgetq_lane_u32(v_uiSum1, i) + vgetq_lane_u32(v_uiSum2, i) +
-      vgetq_lane_u32(v_uiSum3, i) + vgetq_lane_u32(v_uiSum4, i);
-  }
+  Distortion uiSum =
+    vgetq_lane_u32(v_uiSum1, 0) + vgetq_lane_u32(v_uiSum2, 0) +
+    vgetq_lane_u32(v_uiSum3, 0) + vgetq_lane_u32(v_uiSum4, 0) +
+    vgetq_lane_u32(v_uiSum1, 1) + vgetq_lane_u32(v_uiSum2, 1) +
+    vgetq_lane_u32(v_uiSum3, 1) + vgetq_lane_u32(v_uiSum4, 1) +
+    vgetq_lane_u32(v_uiSum1, 2) + vgetq_lane_u32(v_uiSum2, 2) +
+    vgetq_lane_u32(v_uiSum3, 2) + vgetq_lane_u32(v_uiSum4, 2) +
+    vgetq_lane_u32(v_uiSum1, 3) + vgetq_lane_u32(v_uiSum2, 3) +
+    vgetq_lane_u32(v_uiSum3, 3) + vgetq_lane_u32(v_uiSum4, 3);
 
   uiSum <<= iSubShift;
   return ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT(pcDtParam->bitDepth-8) );
