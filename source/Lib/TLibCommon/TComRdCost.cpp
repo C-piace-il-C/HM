76364065fe689115a101f5fa7f6fe7e6505ca9df
Emanuele Canessa <ema0185@gmail.com>
2015-08-26 01:39:50 +0200

    xGetSAD12: unroll prologue

--- source/Lib/TLibCommon/TComRdCost.cpp
+++ source/Lib/TLibCommon/TComRdCost.cpp
@@ -686,13 +686,15 @@ Distortion TComRdCost::xGetSAD12( DistParam* pcDtParam )
     piCur += iStrideCur;
   }
 
-  Distortion uiSum = 0;
-  for (int i = 0; i < 4; i++)
-  {
-    uiSum +=
-      vgetq_lane_u32(v_uiSum1, i) + vgetq_lane_u32(v_uiSum2, i) +
-      vgetq_lane_u32(v_uiSum3, i);
-  }
+  Distortion uiSum =
+    vgetq_lane_u32(v_uiSum1, 0) + vgetq_lane_u32(v_uiSum2, 0) +
+    vgetq_lane_u32(v_uiSum3, 0) +
+    vgetq_lane_u32(v_uiSum1, 1) + vgetq_lane_u32(v_uiSum2, 1) +
+    vgetq_lane_u32(v_uiSum3, 1) + 
+    vgetq_lane_u32(v_uiSum1, 2) + vgetq_lane_u32(v_uiSum2, 2) +
+    vgetq_lane_u32(v_uiSum3, 2) + 
+    vgetq_lane_u32(v_uiSum1, 3) + vgetq_lane_u32(v_uiSum2, 3) +
+    vgetq_lane_u32(v_uiSum3, 3);
 
   uiSum <<= iSubShift;
   return ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT(pcDtParam->bitDepth-8) );
