a3514fc41b6df0cf98bf9958b3a305e066fdd5d8
Andrea Luzzati <andrea.luzzati@gmail.com>
2015-08-27 21:07:14 +0200

    optimizations.h: added clip2_m0s for short values

--- source/optimizations.h
+++ source/optimizations.h
@@ -11,7 +11,7 @@
 #ifdef __arm__
 __inline int clip2(int x, const int min, const int max)
 {
-  asm(
+  asm (
     "CMP %0, %1 \n"
     "ITEE LT \n"
     "MOVLT %0, %1 \n"
@@ -25,16 +25,31 @@ __inline int clip2(int x, const int min, const int max)
 // NOTA: questa è meglio del C in quanto il C non è così furbo da fare ITT GE
 __inline int clip2_m0(int x, const int max)
 {
-  asm(
+  asm (
     "BICS %0, %0, %0, ASR #31 \n"
-    "ITT GE \n"
-    "CMPGE %0, %1 \n"
-    "MOVGE %0, %1 \n"
+    "ITT PL \n"
+    "CMPPL %0, %1 \n"
+    "MOVPL %0, %1 \n"
     : "+r"(x) : "r"(max) : "cc"
-    );
+  );
   return(x);
 }
+
+__inline short clip2_m0s(short x, const short max)
+{
+  asm (
+    "MOV R0, %0, LSL #16 \n"
+    "BICS %0, %0, R0, ASR #31 \n"
+    "ITT PL \n"
+    "CMPPL %0, %1 \n"
+    "MOVPL %0, %1 \n"
+    : "+r"(x) : "r"(max) : "cc","r0"
+  );
+  return(x);
+}
+
 #else
+	
 __inline int clip2(int x, const int min, const int max)
 {
   return( x < min ? min : (x > max ? max : x) );
